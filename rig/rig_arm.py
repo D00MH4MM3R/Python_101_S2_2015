55f574cf32eeb1e2739a01ecc9d8157c672d2458
Jen3d <thejen3d@gmail.com>
2015-09-27 15:32:09 -0400

    Update
    I added the pole vector and the settings control and its connected to
    the switch. Cleaned up the rig a bit more. Will come back to the control
    creation later to make the controls more pretty.

--- rig/rig_arm.py
+++ rig/rig_arm.py
@@ -18,12 +18,6 @@ cmds.joint(n = 'elbowFK_jnt', p = [3, 0, 0])
 cmds.joint(n = 'wristFK_jnt', p = [6, 0, 0])
 cmds.select(d = True)
 
-#Create Result Joints
-cmds.joint(n = 'shoulderResult_jnt', p = [0, 0, 0], o = [180, 0, 0])
-cmds.joint(n = 'elbowResult_jnt', p = [3, 0, 0])
-cmds.joint(n = 'wristResult_jnt', p = [6, 0, 0])
-cmds.select(d = True)
-
 #
 #IK Setup
 #
@@ -39,6 +33,19 @@ cmds.setAttr('armIK_ctrl.rotateZ', 90)
 cmds.makeIdentity(a = True, t = 1, r = 1, s = 1, n = 0, pn = 1)
 cmds.delete('armIK_ctrl', ch = True)
 cmds.parent('armIK_HDL', 'armIK_ctrl')
+
+#Create Pole Vector
+pvCtrlPos = cmds.xform('elbowIK_jnt', q = True, t = True, ws = True)
+pvCtrlx = pvCtrlPos[0]
+pvCtrly = pvCtrlPos[1]
+pvCtrlz = pvCtrlPos[2]
+cmds.circle(n ='armPV_ctrl', nr = (0, 1, 0), c = (0, 0, 0))
+cmds.xform('armPV_ctrl', t = (pvCtrlx, pvCtrly, pvCtrlz - 5), ws = True)
+cmds.setAttr('armPV_ctrl.rotateZ', 90)
+cmds.setAttr('armPV_ctrl.rotateX', 90)
+cmds.makeIdentity(a = True, t = 1, r = 1, s = 1, n = 0, pn = 1)
+cmds.delete('armPV_ctrl', ch = True)
+cmds.poleVectorConstraint('armPV_ctrl', 'armIK_HDL')
 
 
 #
@@ -88,7 +95,7 @@ cmds.parent('wristFK_ctrlShape', 'wristFK_jnt', r = True, s = True)
 cmds.delete('wristFK_ctrl')
 
 #
-#Connect IK and FK joints to Result
+#Connect IK and FK joints to Bind
 #
 armpart = ['shoulder', 'elbow', 'wrist']
 
@@ -98,17 +105,27 @@ for x in range(0, len(armpart)):
     cmds.setAttr('left_' + armpart[x] + '_rot_IkFkChoice.blender', 1)
     cmds.connectAttr(armpart[x] + 'IK_jnt.rotate', 'left_' + armpart[x] + '_rot_IkFkChoice.color1')
     cmds.connectAttr(armpart[x] + 'FK_jnt.rotate', 'left_' + armpart[x] + '_rot_IkFkChoice.color2')
-    cmds.connectAttr('left_' + armpart[x] + '_rot_IkFkChoice.output', armpart[x] + 'Result_jnt.rotate')
+    cmds.connectAttr('left_' + armpart[x] + '_rot_IkFkChoice.output', armpart[x] + 'Bind_jnt.rotate')
 
     #Connect translates from IK FK to blend to result on the arm
     cmds.createNode('blendColors', n = 'left_' + armpart[x] + '_trans_IkFkChoice')
     cmds.setAttr('left_' + armpart[x] + '_trans_IkFkChoice.blender', 1)
     cmds.connectAttr(armpart[x] + 'IK_jnt.translate', 'left_' + armpart[x] + '_trans_IkFkChoice.color1')
     cmds.connectAttr(armpart[x] + 'FK_jnt.translate', 'left_' + armpart[x] + '_trans_IkFkChoice.color2')
-    cmds.connectAttr('left_' + armpart[x] + '_trans_IkFkChoice.output', armpart[x] + 'Result_jnt.translate')
-    
-for y in range(0, len(armpart)):    
-    #Connect Result Rotate to Bind rotate
-    cmds.connectAttr(armpart[y] + 'Result_jnt.rotate', armpart[y] + 'Bind_jnt.rotate')
-    #Connect Result Translate to Bind Translate
-    cmds.connectAttr(armpart[y] + 'Result_jnt.translate', armpart[y] + 'Bind_jnt.translate')
+    cmds.connectAttr('left_' + armpart[x] + '_trans_IkFkChoice.output', armpart[x] + 'Bind_jnt.translate')
+
+#Make settings control for IK FK Switch
+settingsCtrlPos = cmds.xform('wristBind_jnt', q = True, t = True, ws = True)
+setctrlx = settingsCtrlPos[0]
+setctrly = settingsCtrlPos[1]
+setctrlz = settingsCtrlPos[2]
+cmds.circle(n ='settings_ctrl', nr = (0, 1, 0), c = (0, 0, 0))
+cmds.xform('settings_ctrl', t = (setctrlx + 5, setctrly, setctrlz), ws = True)
+cmds.setAttr('settings_ctrl.rotateZ', 90)
+cmds.makeIdentity(a = True, t = 1, r = 1, s = 1, n = 0, pn = 1)
+cmds.delete('settings_ctrl', ch = True)
+cmds.parentConstraint('wristBind_jnt', 'settings_ctrl', mo = True)
+cmds.addAttr(ln = 'ikfkswitch', nn = 'IK FK Switch', at = 'double', min = 0, max = 1, dv = 0, k = True)
+for y in range(0, len(armpart)):
+    #Connect switch attribute to blendcolor nodes
+    cmds.connectAttr('settings_ctrl.ikfkswitch', 'left_' + armpart[y] + '_rot_IkFkChoice.blender')
