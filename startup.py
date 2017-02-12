3bfe9ef384fd9fa31f78b30054a50b9a3f434a03
Ryan Griffin <ryangrif@gmail.com>
2017-02-12 15:16:09 -0500

    Pushing from  master

--- startup.py
+++ startup.py
@@ -1,11 +1,20 @@
-import pymel.core as pm 
+import maya.cmds as cmds
+import os
+import system.utils as utils
+reload(utils)
 
-print 'Startup'
+print "Startup"
+
 
 # Change the current time unit to ntsc
-pm.currentUnit(time='ntsc')
+cmds.currentUnit( time='ntsc' )
 
-# Change the current linear unit to centimeters
-pm.currentUnit(linear='cm')
+# Change the current linear unit to inches
+cmds.currentUnit( linear='cm' )
+
+# Set a system path to files.  We can do this with the os module
+os.environ["RDOJO_DATA"] = 'C:/Users/Griffy/Documents/GitHub/Python_101_S2_2015/'
 
 import ui.ui as ui
+reload(ui)
+ui.RDojo_UI()
